f756738510edec779a5880adced95bfa5f7f63dc
naomi <zixinli2022@163.com>
2023-10-26 19:02:04 +0800

    update ListingController, ListingRepository and ListingService

--- web-service/src/main/java/com/example/webservice/controller/ListingController.java
+++ web-service/src/main/java/com/example/webservice/controller/ListingController.java
@@ -1,73 +1,56 @@
 package com.example.webservice.controller;
 
 import com.example.webservice.model.Listing;
-import com.example.webservice.repository.ListingRepository;
+import com.example.webservice.service.ListingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/listings")
+@RequestMapping("/listing")
 public class ListingController {
 
     @Autowired
-    private ListingRepository listingRepository;
+    private ListingService listingService;
 
-    // GET request to retrieve all listings
     @GetMapping
     public ResponseEntity<List<Listing>> getAllListings() {
-        return new ResponseEntity<>(listingRepository.findAll(), HttpStatus.OK);
+        return ResponseEntity.ok(listingService.getAllListings());
     }
 
-    // GET request to retrieve a listing by its ID
     @GetMapping("/{id}")
     public ResponseEntity<Listing> getListingById(@PathVariable Long id) {
-        return listingRepository.findById(id)
-                .map(listing -> new ResponseEntity<>(listing, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        return listingService.getListingById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    // POST request to create a new listing
     @PostMapping("/create")
     public ResponseEntity<Listing> createListing(@RequestBody Listing listing) {
-        Listing newListing = listingRepository.save(listing);
-        return new ResponseEntity<>(newListing, HttpStatus.CREATED);
+        // We can include validations here for constraints if needed
+        return ResponseEntity.ok(listingService.createListing(listing));
     }
 
-    // PUT request to update a listing by its ID
     @PutMapping("/update/{id}")
     public ResponseEntity<Listing> updateListing(@PathVariable Long id, @RequestBody Listing updatedListing) {
-        if (listingRepository.existsById(id)) {
-            updatedListing.setListingID(id);
-            Listing savedListing = listingRepository.save(updatedListing);
-            return new ResponseEntity<>(savedListing, HttpStatus.OK);
-        }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return listingService.updateListing(id, updatedListing)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    // DELETE request to delete a listing by its ID
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteListing(@PathVariable Long id) {
-        if (listingRepository.existsById(id)) {
-            listingRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        if (listingService.deleteListing(id)) {
+            return ResponseEntity.noContent().build();
         }
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return ResponseEntity.notFound().build();
     }
 
-    // GET request to search listings based on latitude and longitude
+    // If the location-based search gets implemented
     @GetMapping("/search")
     public ResponseEntity<List<Listing>> searchListingsByLocation(@RequestParam Double latitude, @RequestParam Double longitude) {
-        // This method in the repository would still need to be defined.
-        return new ResponseEntity<>(listingRepository.findListingsByLocation(latitude, longitude), HttpStatus.OK);
-    }
-
-    // Handle exceptions
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.ok(listingService.searchListingsByLocation(latitude, longitude));
     }
 }
