0c71ed927bd79aac11e4176a4b4543b1a4a42fb7
naomi <zixinli2022@163.com>
2023-10-26 20:56:37 +0800

    added tests for FacilityControllor and ListingController

--- web-service/src/test/java/com/example/webservice/controller/ListingControllerTest.java
+++ web-service/src/test/java/com/example/webservice/controller/ListingControllerTest.java
@@ -1,67 +1,132 @@
 package com.example.webservice.controller;
 
 import com.example.webservice.model.Listing;
-import com.example.webservice.repository.ListingRepository;
+import com.example.webservice.service.ListingService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(ListingController.class)
 public class ListingControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private ListingRepository listingRepository;
+    private ListingService listingService;
 
-    private Listing testListing;
+    private ObjectMapper objectMapper;
 
     @BeforeEach
-    public void setup() {
-        testListing = new Listing();
-        testListing.setListingID(1L);  // adjusted to Long type
-        // ... set other fields as necessary ...
-
-        when(listingRepository.findAll()).thenReturn(Arrays.asList(testListing));
-        when(listingRepository.findById(1L)).thenReturn(Optional.of(testListing));  // adjusted to Long type
+    void setUp() {
+        objectMapper = new ObjectMapper();
     }
 
     @Test
-    public void getAllListings_ShouldReturnListings() throws Exception {
-        mockMvc.perform(get("/listings"))
+    public void testGetAllListings() throws Exception {
+        Listing listing = new Listing();
+        // Set properties if necessary
+        when(listingService.getAllListings()).thenReturn(List.of(listing));
+
+        mockMvc.perform(get("/listing"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].listingID").value(1L));
+                .andExpect(content().json(objectMapper.writeValueAsString(List.of(listing))));
     }
 
     @Test
-    public void getListingById_ValidId_ShouldReturnListing() throws Exception {
-        mockMvc.perform(get("/listings/1"))
+    public void testGetListingById() throws Exception {
+        Long id = 1L;
+        Listing listing = new Listing();
+        // Set properties if necessary
+        when(listingService.getListingById(id)).thenReturn(Optional.of(listing));
+
+        mockMvc.perform(get("/listing/{id}", id))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.listingID").value(1L));
+                .andExpect(content().json(objectMapper.writeValueAsString(listing)));
     }
 
     @Test
-    public void getListingById_InvalidId_ShouldReturnNotFound() throws Exception {
-        mockMvc.perform(get("/listings/99"))
+    public void testGetListingById_NotFound() throws Exception {
+        Long id = 1L;
+        when(listingService.getListingById(id)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/listing/{id}", id))
                 .andExpect(status().isNotFound());
     }
 
-    // ... rest of the tests remain the same ...
+    @Test
+    public void testCreateListing() throws Exception {
+        Listing listing = new Listing();
+        // Set properties if necessary
+        when(listingService.createListing(any(Listing.class))).thenReturn(listing);
+
+        mockMvc.perform(post("/listing/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(listing)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(listing)));
+    }
+
+
+    @Test
+    public void testUpdateListing_NotFound() throws Exception {
+        Long id = 1L;
+        Listing updatedListing = new Listing();
+        // Set properties if necessary
+        when(listingService.updateListing(id, updatedListing)).thenReturn(Optional.empty());
+
+        mockMvc.perform(put("/listing/update/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedListing)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testDeleteListing() throws Exception {
+        Long id = 1L;
+        when(listingService.deleteListing(id)).thenReturn(true);
+
+        mockMvc.perform(delete("/listing/delete/{id}", id))
+                .andExpect(status().isNoContent());
+
+        verify(listingService, times(1)).deleteListing(id);
+    }
+
+    @Test
+    public void testDeleteListing_NotFound() throws Exception {
+        Long id = 1L;
+        when(listingService.deleteListing(id)).thenReturn(false);
+
+        mockMvc.perform(delete("/listing/delete/{id}", id))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testSearchListingsByLocation() throws Exception {
+        Double latitude = 40.0;
+        Double longitude = 50.0;
+        Double range = 10.0;
+        Listing listing = new Listing();
+        // Set properties if necessary
+        when(listingService.searchListingsByLocation(latitude, longitude, range)).thenReturn(List.of(listing));
+
+        mockMvc.perform(get("/listing/search")
+                        .param("latitude", String.valueOf(latitude))
+                        .param("longitude", String.valueOf(longitude))
+                        .param("range", String.valueOf(range)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(List.of(listing))));
+    }
 }
