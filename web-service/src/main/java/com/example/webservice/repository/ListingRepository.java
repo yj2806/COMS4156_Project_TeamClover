5b63d81f31fc630ea42b1b92b6a9c60bd9d12a37
naomi <zixinli2022@163.com>
2023-11-25 02:34:47 +0800

    listing update - add new parameter in listing search

--- web-service/src/main/java/com/example/webservice/repository/ListingRepository.java
+++ web-service/src/main/java/com/example/webservice/repository/ListingRepository.java
@@ -11,10 +11,29 @@ import java.util.List;
 @Repository
 public interface ListingRepository extends JpaRepository<Listing, Long> {
 
-    @Query(value = "SELECT l.* FROM listings l JOIN facility f ON l.associated_facilityID = f.facilityID WHERE ABS(f.latitude - :latitude) < :range AND ABS(f.longitude - :longitude) < :range", nativeQuery = true)
-    List<Listing> findListingsByLocation(@Param("latitude") Double latitude, @Param("longitude") Double longitude, @Param("range") Double range);
+//    @Query(value = "SELECT l.* FROM listings l JOIN facility f ON l.associated_facilityID = f.facilityID WHERE ABS(f.latitude - :latitude) < :range AND ABS(f.longitude - :longitude) < :range", nativeQuery = true)
+//    List<Listing> findListingsByLocation(@Param("latitude") Double latitude, @Param("longitude") Double longitude, @Param("range") Double range);
 
     @Query(value = "SELECT l.* FROM listings l WHERE l.associated_facilityID = :facilityID", nativeQuery = true)
     List<Listing> findListingsByFacilityID(@Param("facilityID") Long facilityID);
+
+    @Query(value = "SELECT l.* FROM listings l JOIN facility f ON l.associated_facilityID = f.facilityID " +
+            "WHERE (:isPublic IS NULL OR l.is_public = :isPublic) " +
+            "AND (:groupCode IS NULL OR l.group_code = :groupCode) " +
+            "AND (:itemList IS NULL OR l.item_list = :itemList) " +
+            "AND (:ageRequirement IS NULL OR l.age_requirement = :ageRequirement) " +
+            "AND (:veteranStatus IS NULL OR l.veteran_status = :veteranStatus) " +
+            "AND (:gender IS NULL OR l.gender = :gender) " +
+            "AND ABS(f.latitude - :latitude) < :range AND ABS(f.longitude - :longitude) < :range",
+            nativeQuery = true)
+    List<Listing> findListingsByCriteria(@Param("isPublic") Boolean isPublic,
+                                         @Param("groupCode") Integer groupCode,
+                                         @Param("itemList") String itemList,
+                                         @Param("ageRequirement") Integer ageRequirement,
+                                         @Param("veteranStatus") Boolean veteranStatus,
+                                         @Param("gender") String gender,
+                                         @Param("latitude") Double latitude,
+                                         @Param("longitude") Double longitude,
+                                         @Param("range") Double range);
 
 }
