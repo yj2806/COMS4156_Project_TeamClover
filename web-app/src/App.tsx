d99e018a7ea478848c87f8ada36f93558535b6aa
Xinyi Liu <xinyil@Xinyis-MacBook-Pro.local>
2023-11-27 22:31:18 -0500

    wip: add menu

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
+import Client from './pages/ClientCreate';
 
 const App = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Homepage />} />
+                <Route path="/client/create" element={<Client />} />
             </Routes>
         </>
     );
--- web-app/src/pages/PageLayout.tsx
+++ web-app/src/pages/PageLayout.tsx
@@ -2,39 +2,101 @@ import {
     AppBar,
     Box,
     CssBaseline,
+    Divider,
     Drawer,
     List,
     ListItem,
     ListItemButton,
     ListItemIcon,
     ListItemText,
-    Stack,
     Typography,
 } from '@mui/material';
 import React from 'react';
 
-const PageLayout: React.FC = () => {
+type Props = {
+    children?: React.ReactNode;
+};
+
+type MenuListing = {
+    name: string;
+    href: string;
+};
+const PageLayout: React.FC<Props> = ({ children }) => {
+    const drawerWidth = 240;
+    const clientMenuItems: MenuListing[] = [
+        { name: 'Client Create', href: '/client/create' },
+        { name: 'Client Edit', href: '/client/edit' },
+    ];
+    const listingMenuItems: MenuListing[] = [
+        { name: 'Listing Create', href: '/listing/create' },
+        { name: 'View Listing', href: '/listing/create' },
+        { name: 'Edit Listing', href: '/listing/edit' },
+    ];
+
+    const facilityMenuItems: MenuListing[] = [
+        { name: 'Facility Create', href: '/facility/create' },
+        { name: 'View Facility', href: '/facility/view' },
+        { name: 'Edit Facility', href: '/facility/edit' },
+    ];
+
     return (
         <CssBaseline>
-            <Stack sx={{ display: 'flex' }}>
-                <AppBar position="sticky">
+            <Box sx={{ display: 'flex' }}>
+                <AppBar position="fixed" sx={{ width: `calc(100% - ${drawerWidth}px)` }}>
                     <Typography variant="h5" sx={{ p: 2 }}>
                         Team Clove - Front End
                     </Typography>
                 </AppBar>
-                <Drawer sx={{ width: 500 }} variant="permanent" anchor="left">
+                <Drawer
+                    sx={{
+                        width: drawerWidth,
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': {
+                            width: drawerWidth,
+                            boxSizing: 'border-box',
+                        },
+                    }}
+                    variant="permanent"
+                    anchor="left"
+                >
                     <List>
-                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
+                        <ListItem sx={{ fontWeight: 'bold' }}>Client</ListItem>
+                        {clientMenuItems.map((item, index) => (
+                            <ListItem key={item.name} disablePadding>
+                                <ListItemButton href={item.href}>
                                     <ListItemIcon></ListItemIcon>
-                                    <ListItemText primary={text} />
+                                    <ListItemText primary={item.name} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    <List>
+                        <ListItem sx={{ fontWeight: 'bold' }}>Listing</ListItem>
+                        {listingMenuItems.map((item, index) => (
+                            <ListItem key={item.name} disablePadding>
+                                <ListItemButton href={item.href}>
+                                    <ListItemIcon></ListItemIcon>
+                                    <ListItemText primary={item.name} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    <List>
+                        <ListItem sx={{ fontWeight: 'bold' }}>Facility</ListItem>
+                        {facilityMenuItems.map((item, index) => (
+                            <ListItem key={item.name} disablePadding>
+                                <ListItemButton href={item.href}>
+                                    <ListItemIcon></ListItemIcon>
+                                    <ListItemText primary={item.name} />
                                 </ListItemButton>
                             </ListItem>
                         ))}
                     </List>
                 </Drawer>
-            </Stack>
+                <Box>{children}</Box>
+            </Box>
         </CssBaseline>
     );
 };
