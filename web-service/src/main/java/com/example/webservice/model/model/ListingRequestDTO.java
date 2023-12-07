2611c3e025e7ee421b79fc3efc5f0968446ae8a9
Xinyi Liu <xinyil@xinyis-mbp.mynetworksettings.com>
2023-12-07 16:10:23 -0500

    remove listing request CROS

--- web-service/src/main/java/com/example/webservice/model/model/ListingRequestDTO.java
+++ web-service/src/main/java/com/example/webservice/model/model/ListingRequestDTO.java
@@ -7,13 +7,11 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@CrossOrigin
 public class ListingRequestDTO {
 
     @NotNull(message = "Public status is required.")
