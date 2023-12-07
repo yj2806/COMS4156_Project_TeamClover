b22cd35ee46ce2d95b8b92d609c3a40b49ba128e
Yaochen <123121874+ys3593@users.noreply.github.com>
2023-12-06 20:47:51 -0500

    Update WebServiceApplicationTests.java

--- web-service/src/test/java/com/example/webservice/WebServiceApplicationTests.java
+++ web-service/src/test/java/com/example/webservice/WebServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.webservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@Test
 class WebServiceApplicationTests {
 
     @Test
