c4a648e45aad242452d8810e603594f972a217f6
naomi <zixinli2022@163.com>
2023-12-16 23:30:36 +0800

    implement JWT authentication

---
+++ web-service/src/main/java/com/example/webservice/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.example.webservice.repository;
+import com.example.webservice.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+}
